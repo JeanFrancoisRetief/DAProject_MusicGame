180c87ea99eb37ed467c69a315853f9353d7e75a
JeanFrancoisRetief <2458318@students.wits.ac.za>
2024-06-24 19:43:49 +0200

    State Machine Checker V001

--- Assets/Scripts/StateMachineScript.cs
+++ Assets/Scripts/StateMachineScript.cs
@@ -35,12 +35,22 @@ public class StateMachineScript : MonoBehaviour
 
     public State currentState;
 
+    public GameObject Player;
+    public GameObject WinScreenPanel;
+    public GameObject DeathScreenPanel;
+
+    public bool inCombat;
+    public bool inCutscene;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        inCombat = false;
+        inCutscene = false;
+        WinScreenPanel.SetActive(false);
+        DeathScreenPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -48,6 +58,49 @@ public class StateMachineScript : MonoBehaviour
     {
         Debug.Log(ExplorationMusicTYPE + " " + CombatMusicTYPE + " " + VictoryMusicTYPE + " " + DefeatMusicTYPE + " " + NarrativeMusicTYPE);
 
+        CheckIfInCombat();
+        CheckIfInCutscene();
+
+
+        if (inCutscene)
+        {
+            currentState = State.CUTSCENE;
+        }
+        else if (DeathScreenPanel.active == true)
+        {
+            currentState = State.DeathScreen;
+        }
+        else if (WinScreenPanel.active == true)
+        {
+            currentState = State.WinScreen;
+        }
+        else if(inCombat)
+        {
+            currentState = State.COMBAT;
+        }
+        else if(Player.transform.position.y >= 10)  ///player way above the ground  -can use RAYCAST instead
+        {
+            currentState = State.Flying;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            currentState = State.Running;
+        }
+        else
+        {
+            currentState = State.Walking;
+        }
+
+
+    }
+
+    public void CheckIfInCombat()
+    {
+
+    }
+
+    public void CheckIfInCutscene()
+    {
 
     }
 }
