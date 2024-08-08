a0eba47c9b71f6df678a9f8d168aab463b51d00c
JeanFrancoisRetief <2458318@students.wits.ac.za>
2024-08-08 11:49:39 +0200

    Enemy Health Bar

--- Assets/Scripts/Combat/EnemyScript.cs
+++ Assets/Scripts/Combat/EnemyScript.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyScript : MonoBehaviour
 {
     [Header("Enemy Stats")]
     public int Health;
+    public int MaxHealth;
+
+    [Header("Game Objects")]
+    public GameObject EnemyParentObject;
+    public Slider HealthSlider;
+    public GameObject EnemyUICanvas;
+    public Transform TargetCam;
+
 
     [Header("Import Player Stats")]
     public CombatScript Player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        HealthSlider.maxValue = MaxHealth;
     }
 
     // Update is called once per frame
@@ -20,9 +29,11 @@ public class EnemyScript : MonoBehaviour
     {
         if(Health <= 0)
         {
-            gameObject.SetActive(false);
-        }
+            EnemyParentObject.SetActive(false);
 
+        }
+        HealthSlider.value = Health;
+        EnemyUICanvas.transform.LookAt(TargetCam);
 
     }
 
