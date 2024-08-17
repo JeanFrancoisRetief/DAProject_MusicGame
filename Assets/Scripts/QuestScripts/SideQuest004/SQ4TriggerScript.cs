9af30b321aeb582444f7398ec9fd3cfef7ec1589
JeanFrancoisRetief <2458318@students.wits.ac.za>
2024-08-17 12:15:21 +0200

    side quest 4 progress

--- Assets/Scripts/QuestScripts/SideQuest004/SQ4TriggerScript.cs
+++ Assets/Scripts/QuestScripts/SideQuest004/SQ4TriggerScript.cs
@@ -15,6 +15,13 @@ public class SQ4TriggerScript : MonoBehaviour
         SpawnEnemies,
         PlayCutscene,
         PlayDialogue,
+        Choice001,
+        Choice002,
+        Choice003,
+        Choice004,
+        Choice005,
+        Choice006,
+        Choice007,
         END
     }
 
@@ -32,30 +39,64 @@ public class SQ4TriggerScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (TriggerType == Quest_Trigger_Type.START)
+        if (other.tag == "Player")
         {
+            if (TriggerType == Quest_Trigger_Type.START)
+            {
+                sideQuest004Script.StartQuest();
+                gameObject.transform.position = new Vector3(-800, -500, 800);//get it away
+            }
 
+            if (TriggerType == Quest_Trigger_Type.SpawnEnemies)
+            {
+                sideQuest004Script.SpawnEnemies();
+            }
+
+            if (TriggerType == Quest_Trigger_Type.PlayCutscene)
+            {
+                //n.a.
+            }
+
+            if (TriggerType == Quest_Trigger_Type.Choice001)
+            {
+                sideQuest004Script.PlayNarration001();
+            }
+            if (TriggerType == Quest_Trigger_Type.Choice002)
+            {
+                sideQuest004Script.PlayNarration002();
+            }
+            if (TriggerType == Quest_Trigger_Type.Choice003)
+            {
+                sideQuest004Script.PlayNarration003();
+            }
+            if (TriggerType == Quest_Trigger_Type.Choice004)
+            {
+                sideQuest004Script.PlayNarration004();
+            }
+            if (TriggerType == Quest_Trigger_Type.Choice005)
+            {
+                sideQuest004Script.PlayNarration005();
+            }
+            if (TriggerType == Quest_Trigger_Type.Choice006)
+            {
+                sideQuest004Script.PlayNarration006();
+            }
+            if (TriggerType == Quest_Trigger_Type.Choice007)
+            {
+                sideQuest004Script.PlayNarration007();
+            }
+
+            if (TriggerType == Quest_Trigger_Type.PlayDialogue)
+            {
+                sideQuest004Script.PlayDialogue();
+            }
+
+            if (TriggerType == Quest_Trigger_Type.END)
+            {
+                sideQuest004Script.EndQuest();
+            }
         }
-
-        if (TriggerType == Quest_Trigger_Type.SpawnEnemies)
-        {
-
-        }
-
-        if (TriggerType == Quest_Trigger_Type.PlayCutscene)
-        {
-
-        }
-
-        if (TriggerType == Quest_Trigger_Type.PlayDialogue)
-        {
-
-        }
-
-        if (TriggerType == Quest_Trigger_Type.END)
-        {
-
-        }
+            
 
     }
 }
--- Assets/Scripts/QuestScripts/SideQuest004/SideQuest004.cs
+++ Assets/Scripts/QuestScripts/SideQuest004/SideQuest004.cs
@@ -4,15 +4,215 @@ using UnityEngine;
 
 public class SideQuest004 : MonoBehaviour
 {
+    public GameObject Enemies;
+    public GameObject StangeBuilding;
+    [Header("Quest Game Object")]
+    public GameObject ThisQuest;
+    [Header("HUD Script")]
+    public HUD hud;
+    [Header("Master Quest Handler Script")]
+    public MasterQuestHandler masterQuestHandler;
+    [Header("Other Variables")]
+    public GameObject EndQuestTrigger;
+    public GameObject StartQuestTrigger;
+
+    [HideInInspector] public string tutText;
+    [HideInInspector] public string ObjectiveText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ThisQuest.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (ThisQuest.active == true)
+        {
+            hud.QuestTitleText.text = "The butler parable (_side_quest_004_StartCoroutine_Auto)";
+            hud.QuestObjectiveText.text = ObjectiveText;
+            hud.TutorialText.text = tutText;
+        }
     }
+
+
+    public void StartQuest()
+    {
+        ThisQuest.SetActive(true);
+        StartQuestTrigger.SetActive(false);
+        EndQuestTrigger.SetActive(false);
+        ObjectiveText = "Talk to the butler";
+        tutText = "Designation serial number: 427";
+        masterQuestHandler.DeactivateSideQuestStartTriggers();
+
+        StangeBuilding.SetActive(false);
+    }
+
+    public void SpawnEnemies()
+    {
+        Enemies.SetActive(true);
+    }
+
+    //11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+    public void PlayDialogue()
+    {
+        hud.DialoguePanel.SetActive(true);
+        hud.DialogueSpeakerText.text = "427";
+        hud.SubtitlesText.text = "Good. You. Are. Here. Please. Complete. Designated. Task.";
+        Invoke(nameof(DialogueContinue001), 6);
+        Invoke(nameof(DialogueContinue002), 12);
+        Invoke(nameof(DialogueContinue003), 18);
+        Invoke(nameof(EndDialogue), 24);
+    }
+
+    private void DialogueContinue001()
+    {
+        hud.DialogueSpeakerText.text = "808";
+        hud.SubtitlesText.text = "Elaborate. Term. 'Designated. Task.'";
+    }
+
+    private void DialogueContinue002()
+    {
+        hud.DialogueSpeakerText.text = "427";
+        hud.SubtitlesText.text = "The. Help. Are. Getting. Less. Helpful. Each. Cycle. It. Seems. Designated. Task. Description. Follows. Obtain. Rear-Axle-Lubricant. For. Mayor. Bot. 001.";
+    }
+
+    private void DialogueContinue003()
+    {
+        hud.DialogueSpeakerText.text = "808";
+        hud.SubtitlesText.text = "Affirmative.";
+    }
+
+    private void EndDialogue()
+    {
+        hud.DialoguePanel.SetActive(false);
+        ObjectiveText = "Go to the strange building";
+        tutText = "It is next to the mansion";
+        StangeBuilding.SetActive(true);
+    }
+    //11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+    public void EndQuest()
+    {
+        ThisQuest.SetActive(false);
+        hud.QuestTitleText.text = "(_open_world_StartCoroutine_Auto)";
+        hud.QuestObjectiveText.text = "Explore";
+        hud.TutorialText.text = "";
+
+        hud.WinPanel.SetActive(true);
+        hud.WonQuestTitleText.text = "_side_quest_004_StartCoroutine_Auto == The butler-bot did it - a parable";
+        hud.WonQuestObjectivesText.text = "Made meaningful player choices that will impact on the rest of the game\nDefinitely, Probably, Maybe.\nNah. It. Wont.";
+
+        //masterQuestHandler.MQ3StartTrigger.SetActive(true);//start of next main quest
+        masterQuestHandler.ActivateSideQuestStartTriggers();//open world
+    }
+    //11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+    public void PlayNarration001()
+    {
+        hud.DialoguePanel.SetActive(true);
+        hud.DialogueSpeakerText.text = "427";
+        hud.SubtitlesText.text = "When. 808. Came. To. A. Set. Of. Two. Open. Doors. 808. Took. The. Door. On. Its. Left.";
+
+        hud.QuestObjectiveText.text = "Go through the building";
+        hud.TutorialText.text = "";
+
+        Invoke(nameof(EndNarration), 6);
+    }
+
+    public void PlayNarration002()
+    {
+        // 1-0-0-0 go left
+        hud.DialoguePanel.SetActive(true);
+        hud.DialogueSpeakerText.text = "427";
+        hud.SubtitlesText.text = "808. Continued. Towards. The. Storage. Room. Where. It. Found. Two. Containers. 808. Picked. Up. The. Blue. Container.";
+
+        Invoke(nameof(EndNarration), 6);
+    }
+
+    public void PlayNarration003()
+    {
+        // 2-0-0-0 go right
+        hud.DialoguePanel.SetActive(true);
+        hud.DialogueSpeakerText.text = "427";
+        hud.SubtitlesText.text = "808. Took. The. Wrong. Way. 808. Knew. It. Was. Disobeying. Orders. However. 808. Can. Still. Get. Back. On Track. By. Turning. Left.";
+
+        Invoke(nameof(EndNarration), 6);
+    }
+
+    public void PlayNarration004()
+    {
+        // 2-1-0-0 go right - return
+
+        hud.DialoguePanel.SetActive(true);
+        hud.DialogueSpeakerText.text = "427";
+        hud.SubtitlesText.text = "808. Get. Back. On Track. Towards. The. Storage. Room. Where. It. Found. Two. Containers. 808. Picked. Up. The. Blue. Container. Obey. This. Time.";
+
+        Invoke(nameof(EndNarration), 6);
+    }
+
+    public void PlayNarration005()
+    {
+        // 2-2-0-0 go right - go right
+
+        hud.DialoguePanel.SetActive(true);
+        hud.DialogueSpeakerText.text = "427";
+        hud.SubtitlesText.text = "Fine. Here. Is. Some. Enemies. I. Guess... And. 808. Died. Horribly.";
+        SpawnEnemies();
+        StangeBuilding.SetActive(false);
+        EndQuestTrigger.SetActive(true);
+
+        Invoke(nameof(EndNarration), 6);
+    }
+
+    public void PlayNarration006()
+    {
+        // 1-1-0-0 go left - Pick up red (wrong)
+
+        hud.DialoguePanel.SetActive(true);
+        hud.DialogueSpeakerText.text = "427";
+        hud.SubtitlesText.text = "This. Container. Is. Labeled. 'Rear-Acid-Dissolvent.' 808. Thought. He. Could. Pull. A. Prank. On. 001. 808. Was. Incorrect.";
+        SpawnEnemies();
+        StangeBuilding.SetActive(false);
+        EndQuestTrigger.SetActive(true);
+
+        hud.QuestObjectiveText.text = "Return to 427";
+        hud.TutorialText.text = "427 is nearby";
+
+        Invoke(nameof(EndNarration), 6);
+    }
+
+    public void PlayNarration007()
+    {
+        // 1-2-0-0 go left - Pick up blue (correct)
+
+        hud.DialoguePanel.SetActive(true);
+        hud.DialogueSpeakerText.text = "427";
+        hud.SubtitlesText.text = "This. Container. Is. Labeled. Rear-Axle-Lubricant. 808. Proceeded. To. Give. It. To. 427.";
+        StangeBuilding.SetActive(false);
+        EndQuestTrigger.SetActive(true);
+
+        hud.QuestObjectiveText.text = "Return to 427";
+        hud.TutorialText.text = "427 is nearby";
+
+        Invoke(nameof(PlayNarration008), 6);///////////2
+        Invoke(nameof(EndNarration), 12);
+    }
+
+    public void PlayNarration008()
+    {
+        hud.DialoguePanel.SetActive(true);
+        hud.DialogueSpeakerText.text = "427";
+        hud.SubtitlesText.text = "808. Performed. Exceptionally. It. Must. Increase. Its. Proud-Parameter. By. 22%";
+
+        //Invoke(nameof(EndNarration), 6);
+    }
+
+
+    private void EndNarration()
+    {
+        hud.DialoguePanel.SetActive(false);
+    }
+
+    //11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
 }
