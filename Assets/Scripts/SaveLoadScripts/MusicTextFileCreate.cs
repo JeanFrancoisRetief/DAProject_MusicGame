99fe9badadf882a445e4d631949b14a595802aea
JeanFrancoisRetief <2458318@students.wits.ac.za>
2024-09-06 12:53:34 +0200

    Saving music choice data

--- Assets/Scripts/SaveLoadScripts/MusicTextFileCreate.cs
+++ Assets/Scripts/SaveLoadScripts/MusicTextFileCreate.cs
@@ -12,30 +12,43 @@ public class MusicTextFileCreate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CreateTextFile();
+        //CreateTextFile();
+
+        Directory.CreateDirectory(Application.streamingAssetsPath + "/Music_Logs/");
     }
 
     // Update is called once per frame
     void Update()
     {
-        WriteToTextFile();
+        //WriteToTextFile();
     }
 
-    public void CreateTextFile()
+    public void CreateOrWriteTextFile()
     {
-        using (sw = new StreamWriter(Application.dataPath + "/MusicData.txt", true))
+        /*using (sw = new StreamWriter(Application.dataPath + "/MusicData.txt", true))
         {
             sw.WriteLine("Durring the course of the playthrough, this is what the player chose:");
             sw.WriteLine("Exploration, Combat, Victory, Defeat, Cutscenes");
+        }*/
+
+        string txtDocName = Application.streamingAssetsPath + "/Music_Logs/" + "MusicData" + ".txt";
+
+        if(!File.Exists(txtDocName))
+        {
+            File.WriteAllText(txtDocName, "--------MUSIC LOG--------\n\nOrder: Exploration, Combat, Victory, Defeat, Cutscenes\n\n");
         }
+
+        File.AppendAllText(txtDocName, System.DateTime.Now + " - "+ stateMachineScript.ExplorationMusicTYPE + " " + stateMachineScript.CombatMusicTYPE + " " + stateMachineScript.VictoryMusicTYPE + " " + stateMachineScript.DefeatMusicTYPE + " " + stateMachineScript.NarrativeMusicTYPE + "\n");
+
+
     }
 
     public void WriteToTextFile()
     {
         //Debug.Log(ExplorationMusicTYPE + " " + CombatMusicTYPE + " " + VictoryMusicTYPE + " " + DefeatMusicTYPE + " " + NarrativeMusicTYPE);
-        using (sw)
+        /*using (sw)
         {
             sw.WriteLine(stateMachineScript.ExplorationMusicTYPE + " " + stateMachineScript.CombatMusicTYPE + " " + stateMachineScript.VictoryMusicTYPE + " " + stateMachineScript.DefeatMusicTYPE + " " + stateMachineScript.NarrativeMusicTYPE);
-        }
+        }*/
     }
 }
