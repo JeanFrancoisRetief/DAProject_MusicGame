aca68fe17cf96f0fb6552434d18cc85080332bf0
JeanFrancoisRetief <2458318@students.wits.ac.za>
2024-10-08 14:33:42 +0200

    Collectables (no save)

--- Assets/Scripts/QuestScripts/Collectables/C1TriggerScript.cs
+++ Assets/Scripts/QuestScripts/Collectables/C1TriggerScript.cs
@@ -4,24 +4,52 @@ using UnityEngine;
 
 public class C1TriggerScript : MonoBehaviour
 {
+    public GameObject CollectableUICanvas;
+    public Transform TargetCam;
+
+    public GameObject ParentCollectableObj;
+
     [Header("Corresponding Script")]
-    public MainQuest001 mainQuest001Script;
+    public Collectables001 Collectables001Script;
+
+    [Header("Images")]
+    public GameObject[] images;
+    /*public GameObject img001;
+    public GameObject img002;
+    public GameObject img003;
+    public GameObject img004;
+    public GameObject img005;
+    public GameObject img006;
+    public GameObject img007;
+    public GameObject img008;
+    public GameObject img009;
+    public GameObject img010;
+    public GameObject img011;
+    public GameObject img012;*/
+
+    [Header("SET INDEX")]
+    public int CollectableIndex;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        images[CollectableIndex].SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        CollectableUICanvas.transform.LookAt(TargetCam);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        Collectables001Script.collectables[CollectableIndex] = true;
 
+
+        ParentCollectableObj.SetActive(false);
     }
 }
--- Assets/Scripts/QuestScripts/Collectables/Collectables001.cs
+++ Assets/Scripts/QuestScripts/Collectables/Collectables001.cs
@@ -3,16 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Collectables001 : MonoBehaviour
-{
+{                       
+                                 // 0     1      2      3       4     5       6      7     8      9      10     11     12
+    public bool[] collectables = { false, false, false, false, false, false, false, false, false, false, false, false, false };
+
+    public GameObject[] menuItems;
+
+    public GameObject ColectablePanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*for (int i = 0; i < collectables.Length; i++)
+        {
+            if (collectables[i] == true)
+            {
+                collectables[i] = false;
+            }
+        }*/
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void OnCollectableMenuClick()
+    {
+        if (ColectablePanel.active == true)
+        {
+            ColectablePanel.SetActive(false);
+
+        }
+        else
+        {
+            ColectablePanel.SetActive(true);
+            for (int i = 1; i < collectables.Length; i++)
+            {
+                if (collectables[i] == true)
+                {
+                    menuItems[i].SetActive(true);
+                }
+            }
+        }
+
         
     }
 }
