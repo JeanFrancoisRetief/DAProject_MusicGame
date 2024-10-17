282bedbb549c75ffbbaf446f8afc40a244409d8a
JeanFrancoisRetief <2458318@students.wits.ac.za>
2024-10-17 22:18:18 +0200

    Filled Dialogue manager and Dialogue script inspector (code still needed, after MQ2)

--- Assets/Dialogue/Dialogue.cs
+++ Assets/Dialogue/Dialogue.cs
@@ -11,6 +11,81 @@ public class Dialogue : MonoBehaviour
     public AudioSource MQ1_GP_VoiceLine004;
     [Header("MQ2")]
     public AudioSource MQ2_GP_VoiceLine001;
+    [Header("MQ3")]
+    public AudioSource MQ3_GP_VoiceLine001;
+    public AudioSource MQ3_GP_VoiceLine002;
+    public AudioSource MQ3_GP_VoiceLine003;
+    public AudioSource MQ3_GP_VoiceLine004;
+    public AudioSource MQ3_GP_VoiceLine005;
+    [Header("MQ4")]
+    public AudioSource MQ4_GP_VoiceLine001;
+    public AudioSource MQ4_GP_VoiceLine002;
+    public AudioSource MQ4_GP_VoiceLine003;
+    public AudioSource MQ4_GP_VoiceLine004;
+    public AudioSource MQ4_GP_VoiceLine005;
+    [Header("MQ5")]
+    public AudioSource MQ5_GP_VoiceLine001;
+    public AudioSource MQ5_GP_VoiceLine002;
+    public AudioSource MQ5_GP_VoiceLine003;
+    public AudioSource MQ5_GP_VoiceLine004;
+    public AudioSource MQ5_GP_VoiceLine005;
+    public AudioSource MQ5_GP_VoiceLine006;
+    public AudioSource MQ5_GP_VoiceLine007;
+    public AudioSource MQ5_GP_VoiceLine008;
+    public AudioSource MQ5_GP_VoiceLine009;
+    public AudioSource MQ5_GP_VoiceLine010;
+    public AudioSource MQ5_GP_VoiceLine011;
+    public AudioSource MQ5_GP_VoiceLine012;
+    [Header("SQ1")]
+    public AudioSource SQ1_GP_VoiceLine001;
+    public AudioSource SQ1_GP_VoiceLine002;
+    public AudioSource SQ1_GP_VoiceLine003;
+    public AudioSource SQ1_GP_VoiceLine004;
+    public AudioSource SQ1_GP_VoiceLine005;
+    public AudioSource SQ1_GP_VoiceLine006;
+    public AudioSource SQ1_GP_VoiceLine007;
+    public AudioSource SQ1_GP_VoiceLine008;
+    public AudioSource SQ1_GP_VoiceLine009;
+    public AudioSource SQ1_GP_VoiceLine010;
+    public AudioSource SQ1_GP_VoiceLine011;
+    public AudioSource SQ1_GP_VoiceLine012;
+    public AudioSource SQ1_GP_VoiceLine013;
+    public AudioSource SQ1_GP_VoiceLine014;
+    public AudioSource SQ1_GP_VoiceLine015;
+    public AudioSource SQ1_GP_VoiceLine016;
+    public AudioSource SQ1_GP_VoiceLine017;
+    public AudioSource SQ1_GP_VoiceLine018;
+    [Header("SQ2")]
+    public AudioSource SQ2_GP_VoiceLine001;
+    public AudioSource SQ2_GP_VoiceLine002;
+    public AudioSource SQ2_GP_VoiceLine003;
+    public AudioSource SQ2_GP_VoiceLine004;
+    public AudioSource SQ2_GP_VoiceLine005;
+    public AudioSource SQ2_GP_VoiceLine006;
+    public AudioSource SQ2_GP_VoiceLine007;
+    public AudioSource SQ2_GP_VoiceLine008;
+    public AudioSource SQ2_GP_VoiceLine009;
+    [Header("SQ3")]
+    public AudioSource SQ3_GP_VoiceLine001;
+    public AudioSource SQ3_GP_VoiceLine002;
+    public AudioSource SQ3_GP_VoiceLine003;
+    public AudioSource SQ3_GP_VoiceLine004;
+    public AudioSource SQ3_GP_VoiceLine005;
+    public AudioSource SQ3_GP_VoiceLine006;
+    [Header("SQ4")]
+    public AudioSource SQ4_GP_VoiceLine001;
+    public AudioSource SQ4_GP_VoiceLine002;
+    public AudioSource SQ4_GP_VoiceLine003;
+    public AudioSource SQ4_GP_VoiceLine004;
+    public AudioSource SQ4_GP_VoiceLine005;
+    public AudioSource SQ4_GP_VoiceLine006;
+    public AudioSource SQ4_GP_VoiceLine007;
+    public AudioSource SQ4_GP_VoiceLine008;
+    public AudioSource SQ4_GP_VoiceLine009;
+    public AudioSource SQ4_GP_VoiceLine010;
+    public AudioSource SQ4_GP_VoiceLine011;
+    [Header("SQ5")]
+    public AudioSource SQ5_GP_VoiceLine001;
 
 
 
